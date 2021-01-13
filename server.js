8fa97fbfe85febb120220d4f183194c4477c55e1
Yujin <jyooj08@naver.com>
2021-01-13 14:32:08 +0900

    Get diary informations from mysql and show in first page

--- server.js
+++ server.js
@@ -3,15 +3,36 @@ const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'dailydiary'
+});
 
 app.use(express.static(path.normalize(__dirname)));
+connection.connect();
 
 app.get('/', (request, response) => {
-    console.log('test', path.normalize(__dirname+'/html/index.ejs'));
-    fs.readFile(path.normalize(__dirname+'/html/index.ejs'), 'utf8', function(error, data){
-        response.send(ejs.render(data));
-    });
-    console.log(request.query.date);
+    let date = request.query.date;
+    let index = Number(request.query.index) || 0;
+    if(!date){
+        let today = new Date();
+        let month = Number(today.getMonth()) + 1;
+        if(month > 12) month = 1;
+        date = today.getFullYear() + '-' + month + '-' + today.getDate();
+    }
+    console.log('query: ', date, index);
+
+    connection.query(`select * from diary where date='${date}'`, function(error, results, fields){
+        if(error) console.log('ERROR', error);
+        console.log(results);
+        fs.readFile(path.normalize(__dirname+'/html/index.ejs'), 'utf8', function(error, data){
+            response.send(ejs.render(data, {diarys: results}));
+        });
+    })
+    
 });
 
 app.get('/writeDiary', (request, response)=> {
